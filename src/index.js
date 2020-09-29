54b28514513e2198458601aad1814e9aff0f0676
Domask <domask2@mail.ru>
2020-09-29 19:29:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  if(n<0) n *= -1; 
+  let rever =  n.toString().split('');
+  let result = [];
+  for(let i=rever.length-1; i>=0; i--) {
+    if(rever[i])
+     result.push(rever[i])
+ }
+  return result.join('');
 }
